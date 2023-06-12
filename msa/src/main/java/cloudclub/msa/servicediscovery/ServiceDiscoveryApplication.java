daf465fd3657f44551cefa2497b0a9d07459e694
young <jinyoung7165@gmail.com>
2023-06-13 01:25:07 +0900

    [FEAT] Service Discovery

---
+++ msa/src/main/java/cloudclub/msa/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,22 @@
+package cloudclub.msa.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableEurekaServer // NamingServer 자격 부여
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
---
+++ msa/src/test/java/cloudclub/msa/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudclub.msa.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
